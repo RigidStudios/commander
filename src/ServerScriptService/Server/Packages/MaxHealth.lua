41fcb619623665dd432c21c858504707d22f6b6e
RigidStudios <neim448@gmail.com>
2021-03-07 22:11:25 +0400

    Fixed no character/Trailing spaces

--- src/ServerScriptService/Server/Packages/MaxHealth.lua
+++ src/ServerScriptService/Server/Packages/MaxHealth.lua
@@ -6,6 +6,7 @@ local module = {
 
 module.Execute = function(Client, Type, Attachment)			
 	if Type == "command" then
+		local char = module.API.getCharacter(module.API.getPlayerWithName(Attachment))
 		module.API.sendModalToPlayer(Client).Event:Connect(function(Input)
 			if Input ~= false then
 				if char then
--- src/ServerScriptService/Server/Packages/Teleport.lua
+++ src/ServerScriptService/Server/Packages/Teleport.lua
@@ -4,7 +4,7 @@ local module = {
 	Location = "Player",
 }
 
-module.Execute = function(Client, Type, Attachment)			
+module.Execute = function(Client, Type, Attachment)
 	if Type == "command" then
 		local char = module.API.getCharacter(module.API.getPlayerWithName(Attachment))
 		if char then
